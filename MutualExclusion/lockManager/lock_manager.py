43feb47c5c440213e0ce3ba0dda711451c403ee2
Caio_Coldebella <caio.r.coldebella@outlook.com>
2024-06-27 21:45:58 -0300

    feat: add ack flags to Mutual Exclusion

--- MutualExclusion/lockManager/lock_manager.py
+++ MutualExclusion/lockManager/lock_manager.py
@@ -1,20 +1,24 @@
 import pika
-from collections import deque
 
-def lock_manager():
-    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
-    channel = connection.channel()
-    channel.queue_declare(queue='lock_queue')
-    current_lock_holder = None
-    lock_queue = deque()
+class lock_manager():
+    def __init__(self) -> None:
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue='lock_queue')
+        self.current_lock_holder = None
+        self.start()
 
-    def on_request(ch, method, props, body):
-        nonlocal current_lock_holder
+    def start(self):
+        self.channel.basic_consume(queue='lock_queue', on_message_callback=self.on_request, auto_ack=True)
+        print("Lock manager started")
+        self.channel.start_consuming()
+
+    def on_request(self, ch, method, props, body):
         if body.decode('utf-8') == 'request_lock':
             print("Lock requested")
-            print(props)
-            if current_lock_holder is None:
-                current_lock_holder = props.reply_to
+            if self.current_lock_holder is None:
+                print("Lock granted")
+                self.current_lock_holder = props.reply_to
                 ch.basic_publish(
                     exchange='',
                     routing_key=props.reply_to,
@@ -22,24 +26,18 @@ def lock_manager():
                     body='lock_granted'
                 )
             else:
-                lock_queue.append((props.reply_to, props.correlation_id))
+                print("Lock denied")
+                ch.basic_nack(delivery_tag=method.delivery_tag, requeue=True)
                 
         elif body.decode('utf-8') == 'release_lock':
             print("Lock released")
-            current_lock_holder = None
-            if lock_queue:
-                next_lock_holder, corr_id = lock_queue.popleft()
-                current_lock_holder = next_lock_holder
-                ch.basic_publish(
-                    exchange='',
-                    routing_key=next_lock_holder,
-                    properties=pika.BasicProperties(correlation_id=corr_id),
-                    body='lock_granted'
-                )
-
-    channel.basic_consume(queue='lock_queue', on_message_callback=on_request, auto_ack=True)
-    print("Lock manager started")
-    channel.start_consuming()
+            ch.basic_publish(
+                exchange='',
+                routing_key=props.reply_to,
+                properties=pika.BasicProperties(correlation_id=props.correlation_id),
+                body='lock_released'
+            )
+            self.current_lock_holder = None
 
 if __name__ == "__main__":
-    lock_manager()
+    manager = lock_manager()
